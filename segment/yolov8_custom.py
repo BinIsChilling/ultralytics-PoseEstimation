4313f67835dbb7b6bdc4e400b4d4abb89488e0ef
BinIsChilling <auanson2918@gmail.com>
2024-09-16 01:18:36 +1000

    update

--- segment/yolov8_custom.py
+++ segment/yolov8_custom.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8m-pose.pt')
 
-results = model(source="", show=True, conf=0.3, save=True)
+results = model(source="Alex Pereira vs Sean Strickland.mp4", show=True, conf=0.3, save=True)
